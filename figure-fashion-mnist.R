9d5740e7213521d21bd173719f8673f4011a1a89
Toby Dylan Hocking <tdhock5@gmail.com>
2020-10-22 16:50:29 -0700

    cross validation fig

--- figure-fashion-mnist.R
+++ figure-fashion-mnist.R
@@ -22,14 +22,18 @@ for(data.name in names(data.list)){
   test.list <- data.list[[data.name]][["test"]]
   y <- test.list[["y"]]
   some.intensity.dt.list <- list()
+  design.row <- 0
   for(label in unique(y)){
     obs.i.vec <- which(y == label)[1:obs.per.label]
     for(observation in seq_along(obs.i.vec)){
       obs.i <- obs.i.vec[[observation]]
       intensity.mat <- test.list[["x"]][obs.i,,]
       colnames(intensity.mat) <- rownames(intensity.mat) <- 1:nrow(intensity.mat)
+      design.row <- design.row+1
       some.intensity.dt.list[[paste(label, observation)]] <- data.table(
         label, observation,
+        design.row,
+        design.col=1:length(intensity.mat),
         row=as.integer(row(intensity.mat)),
         col=as.integer(col(intensity.mat)),
         intensity=as.numeric(intensity.mat))
@@ -53,9 +57,39 @@ for(data.name in names(data.list)){
       width=7, height=4.5, res=100, units="in")
   print(gg)
   dev.off()
+  gg <- ggplot()+
+    theme(
+      panel.border=element_rect(fill=NA, color="white", size=0.5),
+      panel.spacing=grid::unit(0, "lines"))+
+    coord_cartesian(expand=FALSE)+
+    geom_tile(aes(
+      design.col, design.row, fill=intensity),
+      data=some.intensity.dt)+
+    scale_fill_gradient(low="black", high="white")+
+    scale_y_reverse("Example/row")+
+    scale_x_continuous(
+      "Pixel/feature/column", breaks=c(1, seq(100, 700, by=100), nrow(last.image.dt)))
+  gg <- ggplot()+
+    facet_grid(label ~ .)+
+    theme(
+      panel.border=element_rect(fill=NA, color="white", size=0.5),
+      panel.spacing=grid::unit(0, "lines"))+
+    coord_cartesian(expand=FALSE)+
+    geom_tile(aes(
+      design.col, observation, fill=intensity),
+      data=some.intensity.dt)+
+    scale_fill_gradient(low="black", high="white")+
+    scale_y_reverse("Example/row")+
+    scale_x_continuous(
+      "Pixel/feature/column", breaks=c(1, seq(100, 700, by=100), nrow(last.image.dt)))
+  png(paste0("figure-fashion-mnist-", data.name, "-design.png"),
+      width=7, height=2, res=100, units="in")
+  print(gg)
+  dev.off()
 }
 
 one.breaks <- c(1, breaks.vec)
+last.image.dt <- some.intensity.dt.list[[length(some.intensity.dt.list)]]
 gg <- ggplot()+
   theme(
     panel.border=element_rect(fill=NA, color="white", size=0.5),
@@ -63,7 +97,7 @@ gg <- ggplot()+
   coord_equal(expand=FALSE)+
   geom_tile(aes(
     col, row, fill=intensity),
-    data=some.intensity.dt.list[[length(some.intensity.dt.list)]])+
+    data=last.image.dt)+
   scale_fill_gradient(low="black", high="white")+
   scale_y_reverse("Pixel row index", breaks=one.breaks)+
   scale_x_continuous("Pixel column index", breaks=one.breaks)
@@ -71,3 +105,17 @@ png("figure-fashion-mnist-one-example.png",
     width=3, height=2.3, res=100, units="in")
 print(gg)
 dev.off()
+
+gg <- ggplot()+
+  theme(
+    panel.border=element_rect(fill=NA, color="white", size=0.5),
+    panel.spacing=grid::unit(0, "lines"))+
+  ##coord_equal(expand=FALSE)+
+  geom_tile(aes(
+    design.col, 1, fill=intensity),
+    data=last.image.dt)+
+  scale_fill_gradient(low="black", high="white")+
+  scale_x_continuous(
+    "Pixel index", breaks=c(1, seq(100, 700, by=100), nrow(last.image.dt)))
+print(gg)
+
---
+++ figure-overfitting-paper.R
@@ -0,0 +1,175 @@
+library(data.table)
+library(ggplot2)
+library(keras)
+## from https://github.com/rstudio/keras/issues/937
+if(FALSE){
+  keras::install_keras(version = "2.1.6", tensorflow = "1.5")
+}
+keras::use_implementation("keras")
+keras::use_backend("tensorflow")
+
+n.folds <- 4
+unique.folds <- 1:n.folds
+sim.data.list <- list()
+csv.vec <- Sys.glob("data_*.csv")
+for(csv in csv.vec){
+  pattern <- gsub("data_|.csv", "", csv)
+  sim.dt <-  data.table::fread(csv)
+  set.seed(1)
+  fold.vec <- sample(rep(unique.folds, l=nrow(sim.dt)))
+  sim.data.list[[pattern]] <- data.table(
+    pattern, sim.dt, fold=factor(fold.vec))
+}
+sim.data <- do.call(rbind, sim.data.list)
+
+validation.fold <- 1
+sim.data[, set := ifelse(fold==validation.fold, "validation", "subtrain")]
+sim.data[, folds := ifelse(
+  fold==validation.fold,
+  validation.fold,
+  paste(unique.folds[-validation.fold], collapse=","))]
+sim.gg <- ggplot()+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(. ~ pattern, labeller=label_both)+ 
+  geom_point(aes(
+    x, y),
+    data=sim.data)+
+  xlab("input/feature x")+
+  ylab("output/label y")
+print(sim.gg)
+
+folds.gg <- ggplot()+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(. ~ pattern, labeller=label_both)+ 
+  geom_point(aes(
+    x, y, color=fold),
+    data=sim.data)+
+  xlab("input/feature x")+
+  ylab("output/label y")
+print(folds.gg)
+
+sets.gg <- ggplot()+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(set + folds ~ pattern, labeller=label_both)+ 
+  geom_point(aes(
+    x, y),
+    data=sim.data)+
+  xlab("input/feature x")+
+  ylab("output/label y")
+print(sets.gg)
+
+nnet.pred.dt.list <- list()
+nnet.loss.dt.list <- list()
+nnet.resid.dt.list <- list()
+epochs.vec <- 2^seq(0, 10)
+pattern <- "sin"
+select.pattern <- data.table(pattern)
+pattern.data <- sim.data[select.pattern, on="pattern"]
+set.data <- list()
+for(set.name in unique(pattern.data$set)){
+  set.data[[set.name]] <- with(pattern.data[set==set.name], list(x=x, y=y))
+}
+hidden.units <- 50
+for(epochs in epochs.vec){
+  cat(paste(pattern, hidden.units, epochs, "\n"))
+  set.seed(1)
+  fit <- nnet::nnet(
+    y ~ x,
+    set.data$subtrain,
+    size=hidden.units,
+    linout=TRUE,
+    maxit=epochs)
+  pred.x <- sort(c(sim.data$x, seq(min(sim.data$x), max(sim.data$x), l=200)))
+  nnet.pred.dt.list[[paste(epochs)]] <- data.table(
+    epochs,
+    x=pred.x,
+    pred.y=as.numeric(predict(fit, data.frame(x=pred.x))))
+  pattern.data[, pred.y := as.numeric(predict(fit, pattern.data))]
+  nnet.resid.dt.list[[paste(epochs)]] <- data.table(
+    epochs, pattern.data)
+  nnet.loss.dt.list[[paste(epochs)]] <- data.table(
+    epochs,
+    pattern.data[, .(mse=mean((pred.y-y)^2)), by=set])
+}#for(epoch
+nnet.pred.dt <- do.call(rbind, nnet.pred.dt.list)
+nnet.resid.dt <- do.call(rbind, nnet.resid.dt.list)
+nnet.loss.dt <- do.call(rbind, nnet.loss.dt.list)
+nnet.min.dt <- nnet.loss.dt[
+  set=="validation", .SD[which.min(mse)] ]
+
+to.hilite <- sort(c(
+  epochs.vec[c(3, length(epochs.vec)-1)],
+  nnet.min.dt$epochs))
+hilite.dt <- data.table(
+  epochs=to.hilite,
+  y=c(1,20,1),
+  fit=c("underfit", "optimal", "overfit"))
+hilite.colors <- structure(
+  c("#1B9E77", "#D95F02", "#7570B3"),
+  names=to.hilite)
+gg <- ggplot()+
+  scale_color_manual(values=hilite.colors)+
+  geom_vline(aes(
+    xintercept=epochs, color=factor(epochs)),
+    size=0.7,
+    data=hilite.dt)+
+  geom_text(aes(
+    epochs, y, label=fit, color=factor(epochs)),
+    angle=90,
+    size=3,
+    vjust=1.2,
+    data=hilite.dt)+
+  geom_line(aes(
+    epochs, mse, linetype=set),
+    size=0.7,
+    data=nnet.loss.dt)+
+  scale_x_log10(breaks=10^seq(0, 3))+
+  scale_y_log10("mean squared error")+
+  coord_cartesian(
+    xlim=nnet.loss.dt[, c(min(epochs), max(epochs)*30)])+
+  theme_bw()+
+  theme(
+    legend.position="none",
+    panel.spacing=grid::unit(0, "lines"))+
+  directlabels::geom_dl(aes(
+    epochs, mse, label=set),
+    method=list(cex=0.8, "last.qp"),
+    data=nnet.loss.dt)
+png("figure-overfitting-paper-loss.png", width=2.5, height=2, units="in", res=200)
+print(gg)
+dev.off()
+dput(RColorBrewer::brewer.pal(Inf, "Dark2"))
+
+hilite.pred <- nnet.pred.dt[hilite.dt, on="epochs"]
+hilite.resid <- nnet.resid.dt[hilite.dt, on="epochs"]
+sets.gg <- ggplot()+
+  theme_bw()+
+  theme(
+    legend.position="none",
+    panel.spacing=grid::unit(0, "lines"))+
+  facet_grid(set + folds ~ epochs, labeller=label_both)+ 
+  geom_point(aes(
+    x, y),
+    shape=1,
+    color="grey50",
+    data=pattern.data)+
+  geom_line(aes(
+    x, pred.y, color=factor(epochs)),
+    size=0.8,
+    alpha=0.75,
+    data=hilite.pred)+
+  geom_segment(aes(
+    x, y,
+    xend=x, yend=pred.y),
+    data=hilite.resid)+
+  scale_color_manual(values=hilite.colors)+
+  xlab("input/feature x")+
+  ylab("output/label y")+
+  coord_cartesian(ylim=c(-10, 20))
+png("figure-overfitting-paper.png", width=5, height=3, units="in", res=200)
+print(sets.gg)
+dev.off()
+ 
