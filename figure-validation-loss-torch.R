9d00964c4ee671bfafc8b5846784019e53efbbe3
Toby Dylan Hocking <tdhock5@gmail.com>
2023-03-31 23:44:28 -0700

    dont use dataset/loader

--- figure-validation-loss-torch.R
+++ figure-validation-loss-torch.R
@@ -4,7 +4,7 @@ library(torch)
 source("download.R")
 zip.dt <- data.table::fread("zip.gz")
 zip.feature.dt <- zip.dt[,-1]
-(zip.size <- sqrt(zip.feature.dt))
+(zip.size <- sqrt(ncol(zip.feature.dt)))
 zip.X.array <- array(
   unlist(zip.feature.dt),
   c(nrow(zip.dt), 1, zip.size, zip.size))
@@ -12,8 +12,8 @@ image(zip.X.array[1,,,])
 str(zip.X.array)
 zip.X.tensor <- torch::torch_tensor(zip.X.array)
 str(zip.X.tensor)
-zip.y.array <- array(zip.dt$V1, c(nrow(zip.dt),1))
-zip.y.tensor <- torch::torch_tensor(zip.y.array)
+zip.y.array <- array(zip.dt$V1, c(nrow(zip.dt)))
+zip.y.tensor <- torch::torch_tensor(zip.y.array+1L, torch::torch_long())
 str(zip.y.tensor)
 
 ## some digits to display.
@@ -44,7 +44,8 @@ dev.off()
 
 ## Linear model is defined by a vector of weights, one for each input
 ## feature, and intercept/bias.
-linear.obj <- torch::nn_linear(ncol(zip.feature.dt), 1)
+n.classes <- length(unique(zip.y.array))
+linear.obj <- torch::nn_linear(ncol(zip.feature.dt), n.classes)
 linear.obj$weight
 linear.obj$bias
 linear.obj$weight$grad
@@ -54,7 +55,7 @@ linear.seq <- torch::nn_sequential(
   torch::nn_flatten(),
   linear.obj)
 pred.tensor <- linear.seq(zip.X.tensor)
-loss.fun <- torch::nn_bce_with_logits_loss()
+loss.fun <- torch::nn_cross_entropy_loss()
 loss.tensor <- loss.fun(pred.tensor, zip.y.tensor)
 loss.tensor$backward()
 
@@ -72,7 +73,6 @@ optimizer <- torch::optim_sgd(linear.seq$parameters, lr=step.size)
 linear.obj$bias
 optimizer$step()
 
-
 zip_dataset <- torch::dataset(
   name = "subtrain_dataset",
   initialize = function(X.tensor, y.tensor) {
@@ -80,18 +80,26 @@ zip_dataset <- torch::dataset(
     self$y <- y.tensor
   },
   .getbatch = function(index.vec) {
-    list(X=self$X[index.vec,,,], y=self$y[index.vec,])
+    list(X=self$X[index.vec,,,,drop=FALSE], y=self$y[index.vec])
   },    
   .length = function() {
     self$y$size()
   }
 )
 
+get_loss <- function(L, index.vec=seq_along(L$y)){
+  X <- L$X[index.vec,,,,drop=FALSE]
+  y <- L$y[index.vec]
+  pred <- linear.model(X)
+  loss.fun(pred, y)
+}
+
 n.folds <- 3
 num_epochs <- 50
 uniq.folds <- 1:n.folds
 set.seed(1)
 fold.vec <- sample(rep(uniq.folds, l=nrow(zip.dt)))
+loss.dt.list <- list()
 for(test.fold in uniq.folds){
   is.train <- fold.vec!=test.fold
   train.i <- which(is.train)
@@ -104,24 +112,57 @@ for(test.fold in uniq.folds){
   for(set.name in names(is.set.list)){
     is.set <- is.set.list[[set.name]]
     tensor.list[[set.name]] <- list(
-      X=torch::torch_tensor(zip.X.array[is.set,,,,drop=FALSE]),
-      y=torch::torch_tensor(zip.y.array[is.set,,drop=FALSE]))
+      X=zip.X.tensor[is.set,,,,drop=FALSE],
+      y=zip.y.tensor[is.set])
   }
   subtrain_dataset <- zip_dataset(tensor.list$subtrain$X, tensor.list$subtrain$y)
   subtrain.batch.list <- subtrain_dataset$.getbatch(1:2)
   linear.model <- torch::nn_sequential(
     torch::nn_flatten(),
-    torch::nn_linear(ncol(zip.feature.dt), 1))
+    torch::nn_linear(ncol(zip.feature.dt), n.classes))
   optimizer <- torch::optim_sgd(linear.model$parameters, lr=step.size)
-  subtrain_loader <- torch::dataloader(subtrain_dataset, batch_size=200, shuffle=TRUE)
-  for(epoch in num_epochs){
-    ##TODO
-    coro::loop(for(batch in subtrain_loader){
-      str(batch)
+  batch_size=200
+  subtrain_loader <- torch::dataloader(subtrain_dataset, batch_size=batch_size, shuffle=TRUE)
+  n.subtrain <- length(tensor.list$subtrain$y)
+  for(epoch in 1:num_epochs){
+    batch.vec <- sample(rep(1:batch_size,l=n.subtrain))
+    batch.list <- split(seq_along(batch.vec), batch.vec)
+    for(batch.number in seq_along(batch.list)){
+      batch.indices <- batch.list[[batch.number]]
+      batch.loss <- get_loss(tensor.list$subtrain, batch.indices)
+      optimizer$zero_grad()
+      batch.loss$backward()
+      optimizer$step()
+    }
+    torch::with_no_grad({
+      for(set in c("subtrain", "validation")){
+        set.data <- tensor.list[[set]]
+        set.loss <- get_loss(set.data)
+        loss.dt.list[[paste(test.fold, epoch, set)]] <- print(data.table(
+          test.fold, epoch, set, loss=as.numeric(set.loss)))
+      }
     })
   }
 }
+(loss.dt <- rbindlist(loss.dt.list))
 
+min.dt <- loss.dt[, .SD[which.min(loss)], by=set]
+gg <- ggplot()+
+  ggtitle("Linear model")+
+  theme_bw()+
+  theme(panel.spacing=grid::unit(0, "lines"))+
+  geom_line(aes(
+    epoch, loss, color=set),
+    data=loss.dt)+
+  geom_point(aes(
+    epoch, loss, color=set),
+    data=min.dt)+
+  scale_y_log10("loss value (lower for better predictions)")+
+  scale_x_continuous(
+    "epoch (gradient descent passes through subtrain data)",
+    limits=c(0, num_epochs*1.2),
+    breaks=seq(0, num_epochs, by=10))
+directlabels::direct.label(gg, "right.polygons")
 
 linear.metrics <- linear.model %>% fit(
   zip.X.array, zip.y.mat,
